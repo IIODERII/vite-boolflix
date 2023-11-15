24b5415445aea23faa4cd5f3b0471bd04d41e9ec
IIODERII <oderrisi@gmail.com>
2023-11-15 12:10:34 +0100

    initialize project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>ciao</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+  apiUrl: "https://api.themoviedb.org/3/",
+  api_key: "3d1d44f45c31beb4fa420771967796c4",
+});
