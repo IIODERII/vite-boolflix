43e2efc2cc4e474673a8e4efdda1ccbb24025457
IIODERII <oderrisi@gmail.com>
2023-11-15 14:09:57 +0100

    added dinamic imges

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,56 @@
 <template>
-  <div>ciao</div>
+  <div class="container">
+    <h2>Movies</h2>
+    <div>
+      <div class="row">
+        <div class="col-3" v-for="movie in store.searchedMovieList">
+          <img
+            :src="store.imagePath + movie.poster_path"
+            :alt="movie.title"
+            class="w-100"
+          />
+          {{ movie.title }}
+        </div>
+      </div>
+    </div>
+    <h2>Series</h2>
+    <div>
+      <div class="row">
+        <div class="col-3" v-for="serie in store.searchedSeriesList">
+          <img
+            :src="store.imagePath + serie.poster_path"
+            :alt="serie.name"
+            class="w-100"
+          />
+          {{ serie.name }}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
-export default {};
+import { store } from "./store";
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+  created() {
+    const searchMovieurl = this.store.apiUrl + this.store.endPoint.movie;
+    axios.get(searchMovieurl, { params: this.store.params }).then((resp) => {
+      console.log(resp.data.results);
+      this.store.searchedMovieList = resp.data.results;
+    });
+    const searchSeriesurl = this.store.apiUrl + this.store.endPoint.series;
+    axios.get(searchSeriesurl, { params: this.store.params }).then((resp) => {
+      console.log(resp.data.results);
+      this.store.searchedSeriesList = resp.data.results;
+    });
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
--- src/store.js
+++ src/store.js
@@ -2,5 +2,17 @@ import { reactive } from "vue";
 
 export const store = reactive({
   apiUrl: "https://api.themoviedb.org/3/",
-  api_key: "3d1d44f45c31beb4fa420771967796c4",
+  endPoint: {
+    movie: "search/movie",
+    series: "search/tv",
+  },
+  params: {
+    api_key: "3d1d44f45c31beb4fa420771967796c4",
+    query: "a",
+  },
+  error: "",
+  loading: false,
+  searchedMovieList: [],
+  searchedSeriesList: [],
+  imagePath: "https://image.tmdb.org/t/p/original",
 });
